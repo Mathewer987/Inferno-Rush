5b50fe7d3dfa36b7d1fa018a8c8554f38b48e734
Mathewer987 <mateoaguero31@gmail.com>
2024-08-01 10:37:08 -0300

    Integracion de giro y segumiento de camara al auto

--- Assets/ControllerAuto.cs
+++ Assets/ControllerAuto.cs
@@ -6,7 +6,12 @@ public class ControllerAuto : MonoBehaviour
 {
     public WheelCollider Rueda1;
     public WheelCollider Rueda2;
+    public WheelCollider Rueda3;
+    public WheelCollider Rueda4;
     public int Velocidad = 150;
+    public int Frenar = 50;
+    public Transform Llanta1;
+    public Transform Llanta2;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +22,28 @@ public class ControllerAuto : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rueda1.motorTorque = Velocidad * Input.GetAxis("Vertical");
-        Rueda2.motorTorque = Velocidad * Input.GetAxis("Vertical");
+        Llanta1.localEulerAngles = new Vector3(0, Rueda1.steerAngle, 0);
+        Llanta2.localEulerAngles = new Vector3(0, Rueda2.steerAngle, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        Rueda1.steerAngle = -40 * Input.GetAxis("Horizontal");
+        Rueda2.steerAngle = -40 * Input.GetAxis("Horizontal");
+        Rueda3.motorTorque = Velocidad * Input.GetAxis("Vertical");
+        Rueda4.motorTorque = Velocidad * Input.GetAxis("Vertical");
+
+        if (Input.GetAxis("Vertical") == 0)
+        {
+            Rueda3.brakeTorque = Frenar;
+            Rueda4.brakeTorque = Frenar;
+        }
+
+        else
+        {
+            Rueda3.brakeTorque = 0;
+            Rueda4.brakeTorque = 0;
+
+        }
     }
 }
