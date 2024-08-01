ba93c38faa53c60c75e56ab026b09f25b79f7710
Mathewer987 <mateoaguero31@gmail.com>
2024-08-01 09:54:24 -0300

    Auto avanza (al reves, pero se mueve)

---
+++ Assets/ControllerAuto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerAuto : MonoBehaviour
+{
+    public WheelCollider Rueda1;
+    public WheelCollider Rueda2;
+    public int Velocidad = 150;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rueda1.motorTorque = Velocidad * Input.GetAxis("Vertical");
+        Rueda2.motorTorque = Velocidad * Input.GetAxis("Vertical");
+    }
+}
