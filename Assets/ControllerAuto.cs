7ac1b29f186dbc8d912a2680e26fe425585e2bfa
Mathewer987 <mateoaguero31@gmail.com>
2024-08-02 09:18:31 -0300

    Ruedas se mueven, limite de velocidad e inicio de velocimetro

--- Assets/ControllerAuto.cs
+++ Assets/ControllerAuto.cs
@@ -12,8 +12,20 @@ public class ControllerAuto : MonoBehaviour
     public int Frenar = 50;
     public Transform Llanta1;
     public Transform Llanta2;
+    public Transform Llanta3;
+    public Transform Llanta4;
     public float velocityAct;
     public int VelocityMax = 2000;
+
+    public GameObject LlantaAdDer;
+    public GameObject LlantaAdIzq;
+    public GameObject LlantaAtrDer;
+    public GameObject LlantaAtrIzq;
+
+    float VelocidadAtras;
+
+
+
 
 
     // Start is called before the first frame update
@@ -28,14 +40,28 @@ public class ControllerAuto : MonoBehaviour
         velocityAct = (2 * Mathf.PI * Rueda3.radius) * Rueda3.rpm * 60 / 100;
         Llanta1.localEulerAngles = new Vector3(0, Rueda1.steerAngle, 0);
         Llanta2.localEulerAngles = new Vector3(0, Rueda2.steerAngle, 0);
+        ActualizacionRuedas();
+        VelocidadAtras = velocityAct * -1;
     }
 
     private void FixedUpdate()
     {
+        if (VelocidadAtras < VelocityMax)
+        {
+            Rueda3.motorTorque = Velocidad * Input.GetAxis("Vertical") * -1;
+            Rueda4.motorTorque = Velocidad * Input.GetAxis("Vertical") * -1;
+        }
+
+        else
+        {
+            Rueda3.motorTorque = 0;
+            Rueda4.motorTorque = 0;
+
+        }
+
         Rueda1.steerAngle = 40 * Input.GetAxis("Horizontal");
         Rueda2.steerAngle = 40 * Input.GetAxis("Horizontal");
-        Rueda3.motorTorque = Velocidad * Input.GetAxis("Vertical") * -1;
-        Rueda4.motorTorque = Velocidad * Input.GetAxis("Vertical") * -1;
+       
 
         if (Input.GetAxis("Vertical") == 0)
         {
@@ -49,5 +75,21 @@ public class ControllerAuto : MonoBehaviour
             Rueda4.brakeTorque = 0;
 
         }
+    }
+
+    private void ActualizacionRuedas()
+    {
+        ActualizarPosicionLlantas(Rueda1, Llanta1);
+        ActualizarPosicionLlantas(Rueda2, Llanta2);
+        ActualizarPosicionLlantas(Rueda3, Llanta3);
+        ActualizarPosicionLlantas(Rueda4, Llanta4);
+    }
+
+    private void ActualizarPosicionLlantas(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos;
+        Quaternion quat;
+        collider.GetWorldPose(out pos, out quat);
+        transform.rotation = quat;
     }
 }
