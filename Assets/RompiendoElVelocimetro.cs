980b417cdd25a2ed990888459f3f8862a1ade044
Mathewer987 <mateoaguero31@gmail.com>
2024-08-06 09:32:45 -0300

    Velocimetro mal hecho

---
+++ Assets/RompiendoElVelocimetro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RompiendoElVelocimetro : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public Image imagen; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = rigid.velocity.magnitude;
+        imagen.transform.eulerAngles = new Vector3(0, 0, speed * -5 + 150);
+    }
+}
