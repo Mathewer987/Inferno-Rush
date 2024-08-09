39634442b5fd4b3528935233c27e3f63a93ddf67
Mathewer987 <mateoaguero31@gmail.com>
2024-08-09 09:27:44 -0300

    Video 3 (movimiento de camara)

---
+++ Assets/Camarito.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camarito : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Hijito;
+    public float Velocidad;
+
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Hijito = Player.transform.Find("Camardopolis").gameObject;
+    }
+
+    private void FixedUpdate()
+    {
+        SeguirConLike();
+    }
+
+    private void SeguirConLike()
+    {
+
+        gameObject.transform.position = Vector3.Lerp(transform.position, Hijito.transform.position, Time.deltaTime * Velocidad);
+        gameObject.transform.LookAt(Player.gameObject.transform.position);
+    }
+}
