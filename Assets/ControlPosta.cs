41791512d7d1492f3294c2a96f143e55550a636a
Mathewer987 <mateoaguero31@gmail.com>
2024-08-09 08:44:23 -0300

    2do video (ruedas giran y doblan)

--- Assets/ControlPosta.cs
+++ Assets/ControlPosta.cs
@@ -6,7 +6,9 @@ public class ControlPosta : MonoBehaviour
 {
 
     public WheelCollider[] Ruedas = new WheelCollider[4];
-    public float torque = -200; 
+    public GameObject[] wheelMesh = new GameObject[4];
+    public float motorTorque = -200;
+    public float doblarMax = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,21 +17,46 @@ public class ControlPosta : MonoBehaviour
 
     private void FixedUpdate()
     {
+
+        AnimacionRuedas();
+
         if (Input.GetKey(KeyCode.W))
         {
             for (int i = 0; i < Ruedas.Length; i++)
             {
-                Ruedas[i].motorTorque = torque;
+                Ruedas[i].motorTorque = motorTorque;
+            }
+        }
+        
+        if (Input.GetAxis("Horizontal") !=0)
+        {
+            for (int i = 0; i < Ruedas.Length - 2; i++)
+            {
+                Ruedas[i].steerAngle = Input.GetAxis("Horizontal") * doblarMax;
             }
         }
         else
         {
-            for (int i = 0; i < Ruedas.Length; i++)
+            for (int i = 0; i < Ruedas.Length - 2; i++)
             {
-                Ruedas[i].motorTorque = 0;
+                Ruedas[i].steerAngle = 0;
             }
         }
 
        
+    }
+
+     void AnimacionRuedas()
+    {
+        Vector3 PosicionRueda = Vector3.zero;
+        Quaternion RotacionRueda = Quaternion.identity;
+
+        for (int i = 0; i < 4; i++)
+        {
+            Ruedas[i].GetWorldPose(out PosicionRueda, out RotacionRueda);
+            wheelMesh[i].transform.position = PosicionRueda;
+            wheelMesh[i].transform.rotation = RotacionRueda;
+
+        }
     }
 }
