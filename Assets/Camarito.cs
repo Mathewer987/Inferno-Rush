ec11f91c28088348854290a836cd865f98eb5023
Mathewer987 <mateoaguero31@gmail.com>
2024-08-13 09:17:01 -0300

    Mejore movimiento camara e inicie velocimetro

--- Assets/Camarito.cs
+++ Assets/Camarito.cs
@@ -9,26 +9,42 @@ public class Camarito : MonoBehaviour
     private ControlPosta RR;
     private GameObject Hijito;
     private GameObject cameraConstarint;
-    public float Velocidad;
+    private float Velocidad;
+    public float defaltFOV = 0, desiredFOV = 0;
+    [Range (0,5)]public float tiempoFluido = 0;  
 
     private void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         Hijito = Player.transform.Find("Camardopolis").gameObject;
         RR = Player.GetComponent<ControlPosta>();
+        defaltFOV = Camera.main.fieldOfView;
     }
 
     private void FixedUpdate()
     {
         SeguirConLike();
-
-        Velocidad = (RR.KPH >= 50) ? 20 : RR.KPH / 4;
+        BoosteaFOV();
     }
 
     private void SeguirConLike()
     {
+        Velocidad = Mathf.Lerp(Velocidad, RR.KPH / 2, Time.deltaTime);
 
         gameObject.transform.position = Vector3.Lerp(transform.position, Hijito.transform.position, Time.deltaTime * Velocidad);
         gameObject.transform.LookAt(Player.gameObject.transform.position);
+    }
+
+    private void BoosteaFOV()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, desiredFOV, Time.deltaTime * tiempoFluido);
+        }
+        else
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, defaltFOV, Time.deltaTime * tiempoFluido);
+
+        }
     }
 }
